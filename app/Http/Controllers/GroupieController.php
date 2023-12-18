8165a41f03d303d8f861761cd6f97caed1f5bc5b
YagoSchipper <6022749@mborijnland.nl>
2023-12-18 15:09:49 +0100

    basic zooi groupie controller

--- app/Http/Controllers/GroupieController.php
+++ app/Http/Controllers/GroupieController.php
@@ -15,16 +15,46 @@ class GroupieController extends Controller
 
     public function Show($id)
     {
-
+        $groupie = Groupie::where('user_id', $id)->get();
+        if(!empty($items))
+        {
+            return response()->json($items);
+        }
+        else
+        {
+            return response()->json([
+                "message" => "groupie not found."
+            ], 404);
+        }
     }
 
     public function Store(Request $request)
     {
+        $groupie = new Item;
+        $groupie->name = $request->name;
 
+        return response()->json([
+            "message" => "Groupie Added."
+        ], 201);
     }
 
     public function Update(Request $request, $id)
     {
+        if(Item::where('id', $id)->exists())
+        {
+            $item = Item::find($id);
+            $groupie->name = is_null($request->name) ? $groupie->name : $request->name;
 
+            $groupie->save();
+            return response()->json([
+                "message" => "Groupie Updated."
+            ], 404);
+        }
+        else
+        {
+            return response()->json([
+                "message" => "Groupie Not Found."
+            ], 404);
+        }
     }
 }

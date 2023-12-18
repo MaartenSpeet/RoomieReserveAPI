c5c860124361313dc8f801adf32d120917a3fee4
YagoSchipper <6022749@mborijnland.nl>
2023-12-18 14:35:35 +0100

    item zooi

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
     public function Index()
     {
-
+        $items = Item::all();
+        return response()->json($items);
     }
 
     public function Show($id)
     {
-
+        $item = Item::where('boxie_id', $id);
+        if(!empty($item))
+        {
+            return response()->json($item);
+        }
+        else
+        {
+            return response()->json([
+                "message" => "User not found."
+            ], 404);
+        }
     }
 
     public function Store(Request $request)
